05de0bda63621f3752b7e2e7ac33eec5f0875a46
Felex onyango <felexonyango19@gmail.com>
2022-08-02 15:00:36 +0300

    learning test in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TestingModule } from './testing/testing.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/testing/testing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestingComponent } from '../components/testing/testing.component';
+
+
+@NgModule({
+  declarations: [TestingComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TestingModule { }
